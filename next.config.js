f83c77a2479ed94902940f73883953303c4804d3
blurk <55154720+blurk@users.noreply.github.com>
2021-08-24 11:32:42 +0700

    env

--- next.config.js
+++ next.config.js
@@ -4,13 +4,13 @@ const optimizedImages = require('next-optimized-images')
 const nextConfiguration = {
   target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
   env: {
-    partnerCode: 'MOMOVFKX20210824',
-    accessKey: 'n6JyOolpJrMKtcrm',
-    secretKey: 'QKssHvfcphSUcr2PrpQ4xTNpJNMaSmxt',
-    api: 'https://test-payment.momo.vn/gw_payment/transactionProcessor',
-    templateID: 'template_7t0xpe6',
-    serviceID: 'service_1aj2r6k',
-    userID: 'user_ePY8mJF51VXT3OrkOwMlx',
+    partnerCode: process.env.PARTNERCODE,
+    accessKey: process.env.ACCESSKEY,
+    secretKey: process.env.SECRETKEY,
+    api: process.env.API,
+    templateID: process.env.TEMPLATEID,
+    serviceID: process.env.SERVICEID,
+    userID: process.env.USERID,
   },
 }
 
