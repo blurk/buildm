75bc10e38d5ab5b9a8ea0c2c0d98491988689f99
blurk <55154720+blurk@users.noreply.github.com>
2021-10-12 14:16:30 +0700

    feat: google tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,6 +14,7 @@ class CustomDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap'
             rel='stylesheet'
           />
+          <meta name="google-site-verification" content="sfNZwbRDcd3OgxbMomNDAn5LBHPpnxjq2K4iIwnInS4" />
         </Head>
         <body>
           <Main />
