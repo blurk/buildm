5db9762c8b83617a3badba5f8a973d8673db8e95
blurk <55154720+blurk@users.noreply.github.com>
2021-10-12 20:43:23 +0700

    feat: vietnamese

--- src/components/pricing.js
+++ src/components/pricing.js
@@ -3,28 +3,28 @@ import { Box, Container } from 'theme-ui'
 import BlockTitle from 'components/block-title'
 
 const formatter = new Intl.NumberFormat('it-IT', {
-  style: 'currency',
-  currency: 'VND',
+	style: 'currency',
+	currency: 'VND'
 })
 
 const Pricing = () => {
-  return (
-    <Box sx={styles.pricing} id='pricing'>
-      <Container>
-        <BlockTitle
-          slogan='Hire Us'
-          title={`Start at ${formatter.format(200000)}`}
-          styles={styles.blockTitle}
-        />
-      </Container>
-    </Box>
-  )
+	return (
+		<Box sx={styles.pricing} id='pricing'>
+			<Container>
+				<BlockTitle
+					slogan='Thuê chúng tôi'
+					title={`Đặt cọc trước ${formatter.format(500000)}`}
+					styles={styles.blockTitle}
+				/>
+			</Container>
+		</Box>
+	)
 }
 
 export default Pricing
 
 const styles = {
-  blockTitle: {
-    textAlign: 'center',
-  },
+	blockTitle: {
+		textAlign: 'center'
+	}
 }
