173675bfef5327bd2348a8cde6ed557c94decc70
blurk <55154720+blurk@users.noreply.github.com>
2021-11-10 15:08:28 +0700

    feat: update Logo

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,20 +1,20 @@
 /** @jsx jsx */
 import { jsx, Image } from 'theme-ui'
 import { Link } from 'components/link'
-import logo from 'assets/logo.svg'
+import logo from 'assets/large-removebg-preview.svg'
 
 export default function Logo() {
-  return (
-    <Link
-      path='/'
-      sx={{
-        variant: 'links.logo',
-      }}>
-      <Image
-        src={logo}
-        sx={{ display: 'flex' }}
-        alt='construire-duo team logo'
-      />
-    </Link>
-  )
+	return (
+		<Link
+			path='/'
+			sx={{
+				variant: 'links.logo'
+			}}>
+			<Image
+				src={logo}
+				sx={{ display: 'flex' }}
+				alt='construire-duo team logo'
+			/>
+		</Link>
+	)
 }
